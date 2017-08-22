1979eede1519640d552bc957761f0cae09cd12f5
Patrick Mellon <mellon.patrick.j@gmail.com>
2017-08-21 21:07:22 -0400

    Update user_params method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,7 +15,7 @@ class UsersController < ApplicationController
 
 private
   def user_params
-    params.require(:user).permit(:title,:body,:author_id)
+    params.require(:user).permit(:username,:email,:password)
   end
 
 end
